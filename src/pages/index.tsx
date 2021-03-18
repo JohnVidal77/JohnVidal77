543b0bce8c244c5d1d9bcd8deb0b064f89fcc783
John Vidal <ba.john.vidal@gmail.com>
2021-03-18 00:16:19 -0300

    padding at intro page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Home: FC = () => {
   return (
     <main className="p-8 w-screen h-screen">
-      <div className="bg-white h-full border-2 border-black shadow-hard flex justify-center items-center">
+      <div className="bg-white h-full border-2 border-black shadow-hard flex justify-center items-center p-4">
         <figure className="w-96 h-24">
           <Image src="/logo.png" alt="John Vidal" width={500} height={125} />
         </figure>
