38e46622df82ebb811fcc92be1127efee008f16b
John Vidal <ba.john.vidal@gmail.com>
2021-05-30 01:51:15 -0300

    prettier & eslint added

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,5 +3,5 @@ export default function Home() {
     <div className="w-screen h-screen bg-red-500 grid place-items-center">
       <h1 className="text-3xl">Hello World, John Vidal</h1>
     </div>
-  )
+  );
 }
