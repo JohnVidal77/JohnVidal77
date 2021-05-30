9c80f9f9ac3452e46fbd54e9a11922dead870c2e
John Vidal <ba.john.vidal@gmail.com>
2021-05-30 02:01:36 -0300

    basic style added to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['IBM Plex Mono', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        cyan: '#54FCFC',
+        pink: '#FC5252',
+        black: colors.black,
+        white: colors.white,
+        grey: colors.grey,
+      },
+      boxShadow: {
+        hard: '5px 5px 0 rgba(0, 0, 0)',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
