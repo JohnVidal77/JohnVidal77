0a816e9d83c8228aca9caba62f3bb25b936c8753
John Vidal <ba.john.vidal@gmail.com>
2021-05-30 01:30:28 -0300

    tailwindcss added & basic structure changed

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-red-500 grid place-items-center">
+      <h1 className="text-3xl">Hello World, John Vidal</h1>
+    </div>
+  )
+}
